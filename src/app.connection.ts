9073cb571bf8d314d4a2938e489c39353e73f068
riagie <ogie.nurdiana447@gmail.com>
2024-05-14 17:27:25 +0700

    Initial commit

--- src/app.connection.ts
+++ src/app.connection.ts
@@ -8,31 +8,22 @@ const config = ConfigService();
 
 let AppDataSource: DataSource | undefined;
 
-(async () => {
-  if (config.database.adapter) {
-    try {
-      AppDataSource = await new DataSource({
-        type: "mysql",
-        host: config.database.host,
-        port: config.database.port,
-        username: config.database.username,
-        password: config.database.password,
-        database: config.database.database,
-        synchronize: false,
-        dropSchema: false,
-        logging: false,
-        logger: "file",
-        entities: [__dirname + "/database/**/*.entity.ts"],
-        migrations: [__dirname + "/migrations/**/*.ts"],
-        migrationsTableName: "migration_table",
-      });
-    } catch (error) {
-      console.error(
-        "An error occurred while creating the application data source:",
-        error.message,
-      );
-    }
-  }
-})();
+if (config.database.adapter) {
+  AppDataSource = new DataSource({
+    type: "mysql",
+    host: config.database.host,
+    port: config.database.port,
+    username: config.database.username,
+    password: config.database.password,
+    database: config.database.database,
+    synchronize: false,
+    dropSchema: false,
+    logging: false,
+    logger: "file",
+    entities: [__dirname + "/database/**/*.entity.ts"],
+    migrations: [__dirname + "/migrations/**/*.ts"],
+    migrationsTableName: "migration_table",
+  });
+}
 
 export { AppDataSource };
